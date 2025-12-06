e66425423561a8c24ffbd1b6a7f6baa0fbfa0363
wol20670 <wol20670@naver.com>
2025-12-06 18:56:52 +0900

    seat statue change

--- js/ui/render.js
+++ js/ui/render.js
@@ -3,7 +3,7 @@ export function renderSinglePC(pc) {
   list.innerHTML = `
     <div class="pcbang-card">
       <h3>${pc.name}</h3>
-      <p class="status">좌석 현황: ${pc.available} / ${pc.total}</p>
+      <p class="status">남은 좌석 현황: ${pc.available} / ${pc.total}</p>
       <button onclick="openModal('${pc.id}', '${pc.name}')">좌석 보기</button>
     </div>
   `;
